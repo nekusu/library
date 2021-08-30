a076fbcaf4e9a2c3e70977d05fe82912595fbac1
nekusu <nosoynahuel11@gmail.com>
2021-08-30 05:28:00 -0300

    Add local storage and data deletion

--- js/library.js
+++ js/library.js
@@ -1,10 +1,12 @@
 const addBookButton = document.querySelector('#add-book');
+const clearDataButton = document.querySelector('#clear-data');
 const library = document.querySelector('#library');
 const formContainer = document.querySelector('#form-container');
 const bookForm = formContainer.querySelector('#book-form');
 const submitFormButton = document.querySelector('#submit');
-const books = [];
+const books = JSON.parse(localStorage.getItem('books')) || [];
 let currentId = 0;
+updateLibrary();
 
 class Book {
 	constructor(name, author, pages, read) {
@@ -22,14 +24,13 @@ function toggleBookForm(e) {
 		if (e && e.target !== formContainer) {
 			const formTitle = bookForm.querySelector('#title');
 			const formRead = bookForm.querySelector('#form-read');
-			const submitButton = bookForm.querySelector('#submit');
 			if (e.target === addBookButton) {
 				formTitle.textContent = 'Add a new book';
-				submitButton.value = 'Add';
+				submitFormButton.value = 'Add';
 				formRead.style.display = 'block';
 			} else if (e.target.className === 'edit') {
 				formTitle.textContent = 'Edit book';
-				submitButton.value = 'Edit';
+				submitFormButton.value = 'Edit';
 				formRead.style.display = 'none';
 				currentId = e.target.parentNode.dataset.id;
 				const { name, author, pages, read } = books[currentId];
@@ -45,6 +46,7 @@ function toggleReadCheckbox(e) {
 		books[currentId].read = !books[currentId].read;
 		e.target.title = (books[currentId].read ? '' : 'Not ') + 'Read';
 		e.target.firstElementChild.textContent = books[currentId].read ? 'check_circle_outline' : 'radio_button_unchecked';
+		localStorage.setItem('books', JSON.stringify(books));
 	}
 }
 
@@ -55,6 +57,7 @@ function deleteBook(e) {
 		book = e.target.parentNode.parentNode;
 		book.classList.toggle('hidden');
 		book.addEventListener('transitionend', () => book.remove(), { once: true });
+		localStorage.setItem('books', JSON.stringify(books));
 	}
 }
 
@@ -86,14 +89,15 @@ function setFormValues(form, values) {
 
 function submitForm(e) {
 	e.preventDefault();
-	bookForm.checkValidity();
-	if (bookForm.reportValidity()) {
-		if (e.target.value === 'Add') {
-			books.push(new Book(...getFormValues(bookForm)));
-		} else if (e.target.value === 'Edit') {
-			const values = getFormValues(bookForm);
+	this.checkValidity();
+	if (this.reportValidity()) {
+		if (submitFormButton.value === 'Add') {
+			books.push(new Book(...getFormValues(this)));
+		} else if (submitFormButton.value === 'Edit') {
+			const values = getFormValues(this);
 			Object.assign(books[currentId], { name: values[0], author: values[1], pages: values[2], read: values[3] });
 		}
+		localStorage.setItem('books', JSON.stringify(books));
 		toggleBookForm();
 		updateLibrary();
 	}
@@ -115,16 +119,32 @@ function createBook(name, author, pages, read, id) {
 }
 
 function updateLibrary() {
-	library.innerHTML = '';
-	books.forEach(({ name, author, pages, read }, i) => {
-		const book = createBook(name, author, pages, read, i);
-		library.appendChild(book);
-	});
+	if (books.length) {
+		library.innerHTML = '';
+		clearDataButton.classList.remove('hidden');
+		books.forEach(({ name, author, pages, read }, i) => {
+			const book = createBook(name, author, pages, read, i);
+			library.appendChild(book);
+		});
+	} else {
+		clearDataButton.classList.add('hidden');
+	}
+}
+
+function clearData() {
+	books.splice(0, books.length);
+	localStorage.setItem('books', JSON.stringify(books));
+	for (const book of library.childNodes) {
+		book.classList.toggle('hidden');
+		book.addEventListener('transitionend', () => book.remove(), { once: true });
+	}
+	updateLibrary();
 }
 
 addBookButton.addEventListener('click', toggleBookForm);
+clearDataButton.addEventListener('click', clearData);
 formContainer.addEventListener('click', toggleBookForm);
-submitFormButton.addEventListener('click', submitForm);
+bookForm.addEventListener('submit', submitForm);
 library.addEventListener('click', toggleBookForm);
 library.addEventListener('click', toggleReadCheckbox);
 library.addEventListener('click', deleteBook);
